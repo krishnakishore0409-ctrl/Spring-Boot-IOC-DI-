a6b649efeeeae99e5abcbb27b768fb266ce499d5
krishnakishore0409 <krishnakishore0409@gmail.com>
2026-04-18 15:45:07 +0530

    new Project

--- src/main/java/com/example/TaskController.java
+++ src/main/java/com/example/TaskController.java
@@ -1,50 +1,72 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.example.Task;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.scheduling.config.Task;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
 public class TaskController{
-    
-    @Autowired
-    private TaskService taskService;
 
-   
-    private DummyService dummyService;
-  
     @Autowired
-    public TaskController(DummyService dummyService){
-        this.dummyService = dummyService;
-    }
-
+    TaskService taskService;
 
     @GetMapping("/task")
-    public String getTast() {
+    public String getTask(){
         return taskService.doTask();
     }
 
-    @GetMapping("/myname")
-    public String getname(@RequestParam String name){
+    @GetMapping("/check")
+    public String getMethodName(@RequestParam String name) {
         return taskService.name(name);
+    }
     
+    
+    
+    private List<Task> tasks = new ArrayList<>();
+    
+
+    @PostMapping("/task")
+    public String addTask(@RequestBody Task task) {
+        tasks.add(task);
+        return "Task Added Successfully";
     }
 
-    @GetMapping("/demo/{id}")
-    public String getdemo(@PathVariable int id){
-        return dummyService.demo(id);
+       @PutMapping("/task/{id}")
+    public String updateTask(@PathVariable int id, @RequestBody Task updatedTask) {
+
+        for (Task t : tasks) {
+            if (t.getId() == id) {
+                t.setName(updatedTask.getName());
+                t.setStatus(updatedTask.getStatus());
+                return "Task Updated Successfully";
+            }
+        }
+        return "Task Not Found";
     }
 
-    @GetMapping("/add")
-    public String  add(@RequestParam int a, int b){
-      return dummyService.sum(a,b);
+     @GetMapping("/getTask")
+    public List<Task> getTasks() {
+        return tasks;
     }
-    }
-    
-    
+
+    @DeleteMapping("/deleteTask/{id}")
+     public String deleteTask(@PathVariable  int id){
+                
+            for(Task t : tasks){
+                if(t.getId() == id){
+                    tasks.remove(t);
+                    return "Task delete Successfully";
+                }
+            }
+            return "Not delete Task Id: "+id;
+     }
+
+}
 
